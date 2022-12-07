79821fdeb7398d2da58b62b438c38b216d724b72
alekusa <alekusa@gmail.com>
2022-12-06 21:58:56 -0300

    Modified after change

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,23 +1,36 @@
 import axios from "axios";
 
-export const getPostsRequests = async () => await axios.get("/posts");
+export const getPostsRequests = async () =>
+  await axios.get("https://mernbackend-production-c23a.up.railway.app/posts");
 
-export const getPostRequest = async (id) => await axios.get("/posts/" + id);
+export const getPostRequest = async (id) =>
+  await axios.get(
+    "https://mernbackend-production-c23a.up.railway.app/posts/" + id
+  );
 
 export const deletePostRequest = async (id) =>
-  await axios.delete("/posts/" + id);
+  await axios.delete(
+    "https://mernbackend-production-c23a.up.railway.app/posts/" + id
+  );
 
 export const createPostRequest = async (post) => {
   const form = new FormData();
   for (let key in post) {
     form.append(key, post[key]);
   }
-  return await axios.post("/posts", form, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  return await axios.post(
+    "https://mernbackend-production-c23a.up.railway.app/posts",
+    form,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
 };
 
 export const updatePostRequest = async (id, newFields) =>
-  await axios.put(`/posts/${id}`, newFields);
+  await axios.put(
+    `https://mernbackend-production-c23a.up.railway.app/posts/${id}`,
+    newFields
+  );
